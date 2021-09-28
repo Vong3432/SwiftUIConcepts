78366870a06ff68030c5a69ac56ffa157d75d370
Vong Nyuk Soon <vongnyuksoon2000@gmail.com>
2021-09-28 15:35:52 +0800

    Initial Commit

---
+++ SwiftConcepts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftConcepts
+//
+//  Created by Vong Nyuksoon on 28/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftConcepts/SwiftConceptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConceptsApp.swift
+//  SwiftConcepts
+//
+//  Created by Vong Nyuksoon on 28/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConceptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
